4b8c7735e2fb66215c46d66f1ffe281701b55dd6
lequita1 <quitalouie3@gmail.com>
2026-08-19 16:14:24 +0800

    fixes

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import { create as createAxios } from "axios";
 
 const API_BASE_URL =
-  process.env.EXPO_PUBLIC_API_URL ?? process.env.EXPO_PUBLIC_LOCAL_IP; 
+  process.env.EXPO_PUBLIC_API_URL ??
+  process.env.EXPO_PUBLIC_LOCAL_IP ??
+  "http://localhost:3000"; 
 
 export const api = createAxios({
   baseURL: API_BASE_URL,
