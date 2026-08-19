46e4685446bf72238d160dc5e0f8c9433f386b00
Akarui <AkaruiYuushaProgramming@gmail.com>
2026-08-19 23:55:05 +0800

    W Luigi login and register Icon unification, /app/_layout.jsx adjustment, api.js, authService updates

--- src/app/_layout.jsx
+++ src/app/_layout.jsx
@@ -3,8 +3,6 @@ import { Stack } from "expo-router";
 export default function RootLayout(){
     return (
         <Stack>
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }}/>
-            <Stack.Screen name="(auth)/register" options={{ headerShown: false }}/>
             <Stack.Screen name="(auth)/login" options={{ headerShown: false }}/>
             <Stack.Screen name="(auth)/personal-info" options={{ headerShown: false }}/>
         </Stack>
