22b966e9159ec5fd53cac9e567f975de32ea29c1
tephL <rhezstephen06@gmail.com>
2026-08-22 15:20:08 +0800

    chore(config): fixed metro config

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,4 @@
-import { getDefaultConfig } from 'expo/metro-config';
+const { getDefaultConfig } = require('expo/metro-config.js');
 const config = getDefaultConfig(__dirname);
 
 config.resolver.unstable_enablePackageExports = false;
