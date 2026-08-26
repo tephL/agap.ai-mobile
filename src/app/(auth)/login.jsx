4c3953883885b93389e18d99404f138cdcd5d2a5
tephL <rhezstephen06@gmail.com>
2026-08-27 01:20:12 +0800

    centerized it again

--- src/app/(auth)/login.jsx
+++ src/app/(auth)/login.jsx
@@ -294,7 +294,7 @@ const styles = StyleSheet.create({
   },
   content: {
     flexGrow: 1, 
-    justifyContent: 'center', 
+    paddingTop: 30, 
     paddingHorizontal: 20,
     paddingBottom: 24,
   },
